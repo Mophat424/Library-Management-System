2068ba6fdbb1d4925957c0672d4692dce411738a
USER <USER@DESKTOP-1OETD7S>
2024-07-16 16:19:17 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblAuthor = New System.Windows.Forms.Label()
+        Me.lblYearPublished = New System.Windows.Forms.Label()
+        Me.lblGenre = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtAuthor = New System.Windows.Forms.TextBox()
+        Me.txtYearPublished = New System.Windows.Forms.TextBox()
+        Me.txtGenre = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Lucida Bright", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(28, 35)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(43, 17)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Title"
+        '
+        'lblAuthor
+        '
+        Me.lblAuthor.AutoSize = True
+        Me.lblAuthor.Font = New System.Drawing.Font("Lucida Bright", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAuthor.Location = New System.Drawing.Point(28, 93)
+        Me.lblAuthor.Name = "lblAuthor"
+        Me.lblAuthor.Size = New System.Drawing.Size(60, 17)
+        Me.lblAuthor.TabIndex = 1
+        Me.lblAuthor.Text = "Author"
+        '
+        'lblYearPublished
+        '
+        Me.lblYearPublished.AutoSize = True
+        Me.lblYearPublished.Font = New System.Drawing.Font("Lucida Bright", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYearPublished.Location = New System.Drawing.Point(8, 163)
+        Me.lblYearPublished.Name = "lblYearPublished"
+        Me.lblYearPublished.Size = New System.Drawing.Size(119, 17)
+        Me.lblYearPublished.TabIndex = 2
+        Me.lblYearPublished.Text = "Year Published"
+        '
+        'lblGenre
+        '
+        Me.lblGenre.AutoSize = True
+        Me.lblGenre.Font = New System.Drawing.Font("Lucida Bright", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGenre.Location = New System.Drawing.Point(28, 226)
+        Me.lblGenre.Name = "lblGenre"
+        Me.lblGenre.Size = New System.Drawing.Size(53, 17)
+        Me.lblGenre.TabIndex = 3
+        Me.lblGenre.Text = "Genre"
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(133, 28)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(100, 20)
+        Me.txtTitle.TabIndex = 4
+        '
+        'txtAuthor
+        '
+        Me.txtAuthor.Location = New System.Drawing.Point(133, 90)
+        Me.txtAuthor.Name = "txtAuthor"
+        Me.txtAuthor.Size = New System.Drawing.Size(100, 20)
+        Me.txtAuthor.TabIndex = 5
+        '
+        'txtYearPublished
+        '
+        Me.txtYearPublished.Location = New System.Drawing.Point(133, 163)
+        Me.txtYearPublished.Name = "txtYearPublished"
+        Me.txtYearPublished.Size = New System.Drawing.Size(100, 20)
+        Me.txtYearPublished.TabIndex = 6
+        '
+        'txtGenre
+        '
+        Me.txtGenre.Location = New System.Drawing.Point(133, 226)
+        Me.txtGenre.Name = "txtGenre"
+        Me.txtGenre.Size = New System.Drawing.Size(100, 20)
+        Me.txtGenre.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Lucida Bright", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(30, 314)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Lucida Bright", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(158, 314)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 9
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Lucida Bright", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(294, 314)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 10
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Lucida Bright", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(445, 314)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvBooks.Location = New System.Drawing.Point(278, 20)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.Size = New System.Drawing.Size(511, 226)
+        Me.dgvBooks.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtGenre)
+        Me.Controls.Add(Me.txtYearPublished)
+        Me.Controls.Add(Me.txtAuthor)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblGenre)
+        Me.Controls.Add(Me.lblYearPublished)
+        Me.Controls.Add(Me.lblAuthor)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblYearPublished As Label
+    Friend WithEvents lblGenre As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents dgvBooks As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Data.SqlClient
+
+' Connection String and SQL Connection
+Public Class Form1
+    Private connectionString As String = "Data Source=DESKTOP-1OETD7S;Initial Catalog=LibraryDB;Integrated Security=True"
+    Private connection As SqlConnection
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connection = New SqlConnection(connectionString)
+        LoadBooks()
+    End Sub
+
+    'Loading Data into DataGridView
+    Private Sub LoadBooks()
+        Dim query As String = "SELECT * FROM Books"
+        Dim adapter As New SqlDataAdapter(query, connection)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        dgvBooks.DataSource = table
+    End Sub
+
+    'Code for btnAdd Click Event
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim query As String = "INSERT INTO Books (Title, Author, YearPublished, Genre) VALUES (@Title, @Author, @YearPublished, @Genre)"
+        Dim command As New SqlCommand(query, connection)
+        command.Parameters.AddWithValue("@Title", txtTitle.Text)
+        command.Parameters.AddWithValue("@Author", txtAuthor.Text)
+        command.Parameters.AddWithValue("@YearPublished", Convert.ToInt32(txtYearPublished.Text))
+        command.Parameters.AddWithValue("@Genre", txtGenre.Text)
+
+        connection.Open()
+        command.ExecuteNonQuery()
+        connection.Close()
+        LoadBooks()
+    End Sub
+
+    ' Code for btnUpdate Click Event
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If dgvBooks.SelectedRows.Count > 0 Then
+            Dim id As Integer = Convert.ToInt32(dgvBooks.SelectedRows(0).Cells("ID").Value)
+            Dim query As String = "UPDATE Books SET Title=@Title, Author=@Author, YearPublished=@YearPublished, Genre=@Genre WHERE ID=@ID"
+            Dim command As New SqlCommand(query, connection)
+            command.Parameters.AddWithValue("@ID", id)
+            command.Parameters.AddWithValue("@Title", txtTitle.Text)
+            command.Parameters.AddWithValue("@Author", txtAuthor.Text)
+            command.Parameters.AddWithValue("@YearPublished", Convert.ToInt32(txtYearPublished.Text))
+            command.Parameters.AddWithValue("@Genre", txtGenre.Text)
+
+            connection.Open()
+            command.ExecuteNonQuery()
+            connection.Close()
+            LoadBooks()
+        End If
+    End Sub
+
+
+    'Code for btnDelete Click Event
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        If dgvBooks.SelectedRows.Count > 0 Then
+            Dim id As Integer = Convert.ToInt32(dgvBooks.SelectedRows(0).Cells("ID").Value)
+            Dim query As String = "DELETE FROM Books WHERE ID=@ID"
+            Dim command As New SqlCommand(query, connection)
+            command.Parameters.AddWithValue("@ID", id)
+            connection.Open()
+            command.ExecuteNonQuery()
+            connection.Close()
+            LoadBooks()
+        End If
+
+    End Sub
+
+
+    'Code for btnClear Click Event
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtTitle.Clear()
+        txtAuthor.Clear()
+        txtYearPublished.Clear()
+        txtGenre.Clear()
+
+
+    End Sub
+
+    ' Code for dgvBooks CellClick Event
+    Private Sub dgvBooks_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvBooks.CellContentClick
+
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = dgvBooks.Rows(e.RowIndex)
+            txtTitle.Text = row.Cells("Title").Value.ToString()
+            txtAuthor.Text = row.Cells("Author").Value.ToString()
+            txtYearPublished.Text = row.Cells("YearPublished").Value.ToString()
+            txtGenre.Text = row.Cells("Genre").Value.ToString()
+        End If
+
+
+    End Sub
+End Class
